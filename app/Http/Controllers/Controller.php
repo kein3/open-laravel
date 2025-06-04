1dc6e427db3fc2b7ccaef89e1a0c95ff93e5e742
kein3 <kevin@holiproject.com>
2025-06-04 10:25:07 +0000

    controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
-class HomeController extends Controller
+class Controller extends BaseController
 {
-    public function index()
-    {
-        return view('home');
-    }
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
