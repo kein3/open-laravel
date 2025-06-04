938a48ae48bd96e097b30f11c9e24193c42e1399
kein3 <kevin@holiproject.com>
2025-06-04 10:23:24 +0000

    Correction HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class HomeController extends Controller
 {
-    /**
-     * Display the application home page.
-     */
     public function index()
     {
         return view('home');
