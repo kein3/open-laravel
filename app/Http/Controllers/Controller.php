8119fa64f3cbcca8778a51319ee1d18027b9ed7c
kein3 <kevin@holiproject.com>
2025-06-04 10:14:08 +0000

    Ajout de HomeController et de la vue d'accueil

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('home');
+    }
 }
