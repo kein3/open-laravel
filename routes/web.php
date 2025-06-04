0fce69ab57a5a8ddf6eefced2f6609262823f243
kein3 <kevin@holiproject.com>
2025-06-04 10:19:53 +0000

    Nettoyage et correction de routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,8 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
- HEAD
 use App\Http\Controllers\DashboardController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/dashboard', [DashboardController::class, 'index']);
-
-
-Route::get('/', [HomeController::class, 'index']);
-
- 3ed23f4 (Mise à jour de la route d'accueil)
-
