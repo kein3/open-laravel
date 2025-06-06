766a42d2033081e72d9bfae40756f2b8191c9ca2
kein3 <kevin@holiproject.com>
2025-06-06 07:03:18 +0000

    Supprime tous les fichiers

--- resources/views/components/stat-card.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['title', 'value', 'money' => false])
-
-<div class="bg-white rounded-2xl shadow p-6">
-    <dt class="text-sm font-medium text-gray-500">{{ $title }}</dt>
-    <dd class="mt-1 text-3xl font-semibold text-gray-900">
-        {{ $money ? '€ ' : '' }}{{ $value }}
-    </dd>
-</div>
