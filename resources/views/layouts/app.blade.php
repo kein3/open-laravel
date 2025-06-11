d032326da5f613f5af44f72c85326001086b5b8e
kein3 <kevin@holiproject.com>
2025-06-11 12:59:18 +0000

    sidebarre

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,10 @@
     <!-- Icônes Google Material Symbols -->
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
 </head>
-<body class="bg-gray-50 text-gray-900 antialiased">
+<body 
+<div>*** SIDEBAR LAYOUT TEST ***</div>
+    ...
+class="bg-gray-50 text-gray-900 antialiased">
 <div class="min-h-screen flex">
 
     <!-- SIDEBAR -->
